5b94d451bcd89c60e387eb1946372f279a63a407
Oscar Dominguez <dominguez.celada@gmail.com>
2021-04-19 17:12:24 +0200

    style(lint): apply lint fix

--- experiences/homepage/homepage.test.js
+++ experiences/homepage/homepage.test.js
@@ -1,73 +1,78 @@
-const { bootstrapExperience } = require('../../e2e/utils/bootstrap');
-const { scrollTo } = require('../../e2e/utils/scroll');
-const { touchCard } = require('../../e2e/utils/touch');
-const { isAtSnapPoint } = require('../../e2e/utils/snapPoints');
-const { isCardContentLoaded } = require('../../e2e/utils/cardContent');
-const { expect } = require('chai');
-const { getUrlFixture } = require('../../e2e/utils/fixtureUrl');
-const experience = require('./homepage.json');
+const { bootstrapExperience } = require("../../e2e/utils/bootstrap");
+const { scrollTo } = require("../../e2e/utils/scroll");
+const { touchCard } = require("../../e2e/utils/touch");
+const { isAtSnapPoint } = require("../../e2e/utils/snapPoints");
+const { isCardContentLoaded } = require("../../e2e/utils/cardContent");
+const { expect } = require("chai");
+const { getUrlFixture } = require("../../e2e/utils/fixtureUrl");
+const experience = require("./homepage.json");
 
-describe('homepage experience', function() {
-	let config,
-		fixture;
-	const fixtureUrl = getUrlFixture({
-		siteUrl: 'https://playground.marfeel.com/templates/article-example.html',
-		requestHostname: 'playground.marfeel.com',
-		technology: 'web',
-		experienceUrl: '/experiences/homepage/homepage.json'
-	});
+describe("homepage experience", function () {
+  let config, fixture;
+  const fixtureUrl = getUrlFixture({
+    siteUrl: "https://playground.marfeel.com/templates/article-example.html",
+    requestHostname: "playground.marfeel.com",
+    technology: "web",
+    experienceUrl: "/experiences/homepage/homepage.json",
+  });
 
-	it('setup', async function() {
-		config = experience;
-		fixture = {
-			url: fixtureUrl,
-			articleTitle: 'Article example'
-		};
+  it("setup", async function () {
+    config = experience;
+    fixture = {
+      url: fixtureUrl,
+      articleTitle: "Article example",
+    };
 
-		await bootstrapExperience(browser, config, fixture);
-	});
+    await bootstrapExperience(browser, config, fixture);
+  });
 
-	it('card should render on scroll', async function() {
-		await scrollTo(browser, 400);
+  it("card should render on scroll", async function () {
+    await scrollTo(browser, 400);
 
-		const firstCard = await browser.$(config.cards.homepage.cardSelector);
+    const firstCard = await browser.$(config.cards.homepage.cardSelector);
 
-		const firstCardExists = await firstCard.waitForExist({ timeout: 5000 });
+    const firstCardExists = await firstCard.waitForExist({ timeout: 5000 });
 
-		expect(firstCardExists).equal(true);
-	});
+    expect(firstCardExists).equal(true);
+  });
 
-	it('card should have right content', async function() {
-		const rightContentLoaded = await isCardContentLoaded(browser,
-			config.cards.homepage.cardSelector,
-			config.cards.homepage.content);
+  it("card should have right content", async function () {
+    const rightContentLoaded = await isCardContentLoaded(
+      browser,
+      config.cards.homepage.cardSelector,
+      config.cards.homepage.content
+    );
 
-		expect(rightContentLoaded).equal(true);
-	});
+    expect(rightContentLoaded).equal(true);
+  });
 
-	it('card should be displayed in viewport at initial snap point', async()=>{
-		await scrollTo(browser, 800);
+  it("card should be displayed in viewport at initial snap point", async () => {
+    await scrollTo(browser, 800);
 
-		const firstCard = await browser.$(config.cards.homepage.cardSelector);
+    const firstCard = await browser.$(config.cards.homepage.cardSelector);
 
-		const firstCardIsInViewport = await firstCard.isDisplayedInViewport();
+    const firstCardIsInViewport = await firstCard.isDisplayedInViewport();
 
-		expect(firstCardIsInViewport).equal(true);
+    expect(firstCardIsInViewport).equal(true);
 
-		const isAtInitialSnapPoint = await isAtSnapPoint(browser,
-			config.cards.homepage.cardSelector,
-			config.cards.homepage.snapPoints.initial);
+    const isAtInitialSnapPoint = await isAtSnapPoint(
+      browser,
+      config.cards.homepage.cardSelector,
+      config.cards.homepage.snapPoints.initial
+    );
 
-		expect(isAtInitialSnapPoint).equal(true);
-	});
+    expect(isAtInitialSnapPoint).equal(true);
+  });
 
-	it('activate card by click', async()=>{
-		await touchCard(browser, config.cards.homepage.cardSelector);
+  it("activate card by click", async () => {
+    await touchCard(browser, config.cards.homepage.cardSelector);
 
-		const isAtActiveSnapPoint = await isAtSnapPoint(browser,
-			config.cards.homepage.cardSelector,
-			config.cards.homepage.snapPoints.active);
+    const isAtActiveSnapPoint = await isAtSnapPoint(
+      browser,
+      config.cards.homepage.cardSelector,
+      config.cards.homepage.snapPoints.active
+    );
 
-		expect(isAtActiveSnapPoint).equal(true);
-	});
+    expect(isAtActiveSnapPoint).equal(true);
+  });
 });
