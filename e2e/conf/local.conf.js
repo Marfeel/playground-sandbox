78e28ea1ad99de03cc8ca4bbfec50c5e8388590d
Oscar Dominguez <dominguez.celada@gmail.com>
2021-04-19 17:21:21 +0200

    style(lint): apply lint fix

--- e2e/conf/local.conf.js
+++ e2e/conf/local.conf.js
@@ -1,21 +1,22 @@
-const { buildConfiguration } = require('./configurationBuilder');
+const { buildConfiguration } = require("./configurationBuilder");
 
-const capabilities = [{
-	platformName: 'iOS',
-	automationName: 'XCUITest',
-	deviceName: 'iPhone 8 Plus',
-	platformVersion: '14.4',
-	browserName: 'Safari'
-}];
+const capabilities = [
+  {
+    platformName: "iOS",
+    automationName: "XCUITest",
+    deviceName: "iPhone 8 Plus",
+    platformVersion: "14.4",
+    browserName: "Safari",
+  },
+];
 
 const specs = [
-	// './experiences/branded-content/branded-content-card.test.js'
-	// './experiences/comments/comments.test.js'
-	// './experiences/facebook/facebook.test.js'
-	// './experiences/featured_article/featured_article.test.js'
-	// './experiences/homepage/homepage.test.js'
-	'./experiences/multicard/multicard.test.js'
-
+  // './experiences/branded-content/branded-content-card.test.js'
+  // './experiences/comments/comments.test.js'
+  // './experiences/facebook/facebook.test.js'
+  // './experiences/featured_article/featured_article.test.js'
+  // './experiences/homepage/homepage.test.js'
+  "./experiences/multicard/multicard.test.js",
 ];
 
 const config = buildConfiguration(process.env.E2E_MODE, capabilities, specs);
