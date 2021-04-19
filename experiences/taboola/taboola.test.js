c2ce26b39547ef868ee95ba07c4dada94e6e98e0
Oscar Dominguez <dominguez.celada@gmail.com>
2021-04-19 16:11:26 +0200

    style(lint): add lint fix

--- experiences/taboola/taboola.test.js
+++ experiences/taboola/taboola.test.js
@@ -1,89 +1,90 @@
-const { bootstrapExperience } = require("../../e2e/utils/bootstrap");
-const { scrollTo } = require("../../e2e/utils/scroll");
-const { touchCard } = require("../../e2e/utils/touch");
-const { isAtSnapPoint } = require("../../e2e/utils/snapPoints");
-const { isCardContentLoaded } = require("../../e2e/utils/cardContent");
-const { expect } = require("chai");
-const { getUrlFixture } = require("../../e2e/utils/fixtureUrl");
-const experience = require("./taboola.json");
+const { bootstrapExperience } = require('../../e2e/utils/bootstrap');
+const { scrollTo } = require('../../e2e/utils/scroll');
+const { touchCard } = require('../../e2e/utils/touch');
+const { isAtSnapPoint } = require('../../e2e/utils/snapPoints');
+const { isCardContentLoaded } = require('../../e2e/utils/cardContent');
+const { expect } = require('chai');
+const { getUrlFixture } = require('../../e2e/utils/fixtureUrl');
+const experience = require('./taboola.json');
 
-describe("taboola experience", function () {
-  let config, fixture;
-  const fixtureUrl = getUrlFixture({
-    siteUrl: "https://playground.marfeel.com/templates/article-skeleton.html",
-    requestHostname: "playground.marfeel.com",
-    technology: "web",
-    experienceUrl: "/experiences/taboola/taboola.json",
-  });
+describe('taboola experience', function() {
+	let config,
+		fixture;
+	const fixtureUrl = getUrlFixture({
+		siteUrl: 'https://playground.marfeel.com/templates/article-skeleton.html',
+		requestHostname: 'playground.marfeel.com',
+		technology: 'web',
+		experienceUrl: '/experiences/taboola/taboola.json'
+	});
 
-  it("setup", async function () {
-    config = experience;
-    fixture = {
-      url: fixtureUrl,
-      articleTitle: "Article example",
-    };
+	it('setup', async function() {
+		config = experience;
+		fixture = {
+			url: fixtureUrl,
+			articleTitle: 'Article example'
+		};
 
-    await bootstrapExperience(browser, config, fixture);
-  });
+		await bootstrapExperience(browser, config, fixture);
+	});
 
-  it("card should render on scroll", async function () {
-    await scrollTo(browser, 400);
+	it('card should render on scroll', async function() {
+		await scrollTo(browser, 400);
 
-    const firstCard = await browser.$(config.cards.taboola.cardSelector);
+		const firstCard = await browser.$(config.cards.taboola.cardSelector);
 
-    const firstCardExists = await firstCard.waitForExist({ timeout: 5000 });
+		const firstCardExists = await firstCard.waitForExist({ timeout: 5000 });
 
-    expect(firstCardExists).equal(true);
-  });
+		expect(firstCardExists).equal(true);
+	});
 
-  it("card should have right content", async function () {
-    const rightContentLoaded = await isCardContentLoaded(
-      browser,
-      config.cards.taboola.cardSelector,
-      config.cards.taboola.content
-    );
+	it('card should have right content', async function() {
+		const rightContentLoaded = await isCardContentLoaded(
+			browser,
+			config.cards.taboola.cardSelector,
+			config.cards.taboola.content
+		);
 
-    expect(rightContentLoaded).equal(true);
-  });
+		expect(rightContentLoaded).equal(true);
+	});
 
-  it("card should be displayed in viewport at initial snap point", async () => {
-    await scrollTo(browser, 800);
+	it('card should be displayed in viewport at initial snap point', async() => {
+		await scrollTo(browser, 800);
 
-    const firstCard = await browser.$(config.cards.taboola.cardSelector);
+		const firstCard = await browser.$(config.cards.taboola.cardSelector);
 
-    const firstCardIsInViewport = await firstCard.isDisplayedInViewport();
+		const firstCardIsInViewport = await firstCard.isDisplayedInViewport();
 
-    expect(firstCardIsInViewport).equal(true);
+		expect(firstCardIsInViewport).equal(true);
 
-    const isAtInitialSnapPoint = await isAtSnapPoint(
-      browser,
-      config.cards.taboola.cardSelector,
-      config.cards.taboola.snapPoints.initial
-    );
+		const isAtInitialSnapPoint = await isAtSnapPoint(
+			browser,
+			config.cards.taboola.cardSelector,
+			config.cards.taboola.snapPoints.initial
+		);
 
-    expect(isAtInitialSnapPoint).equal(true);
-  });
+		expect(isAtInitialSnapPoint).equal(true);
+	});
 
-  // fix minimise
-  // it("minimise card and should be at minimised snap point", async ()=>{
-  // 	await dragCardBy(browser, config.cards.taboola.cardSelector, 200)
+	// fix minimise
+	// it("minimise card and should be at minimised snap point", async ()=>{
+	// 	await dragCardBy(browser, config.cards.taboola.cardSelector, 200)
 
-  // 	const isAtMinimisedSnapPoint = await isAtSnapPoint(browser,
-  // 		config.cards.taboola.cardSelector,
-  // 		config.cards.taboola.snapPoints.minimised)
+	// 	const isAtMinimisedSnapPoint = await isAtSnapPoint(browser,
+	// 		config.cards.taboola.cardSelector,
+	// 		config.cards.taboola.snapPoints.minimised)
 
-  // 	expect(isAtMinimisedSnapPoint).equal(true);
-  // });
+	// 	expect(isAtMinimisedSnapPoint).equal(true);
+	// });
 
-  it("activate card by click", async () => {
-    await touchCard(browser, config.cards.taboola.cardSelector);
+	it('activate card by click', async() => {
+		await touchCard(browser, config.cards.taboola.cardSelector);
 
-    const isAtActiveSnapPoint = await isAtSnapPoint(
-      browser,
-      config.cards.taboola.cardSelector,
-      config.cards.taboola.snapPoints.active
-    );
+		const isAtActiveSnapPoint = await isAtSnapPoint(
+			browser,
+			config.cards.taboola.cardSelector,
+			config.cards.taboola.snapPoints.active
+		);
 
-    expect(isAtActiveSnapPoint).equal(true);
-  });
+		expect(isAtActiveSnapPoint).equal(true);
+	});
 });
