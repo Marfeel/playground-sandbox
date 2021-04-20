927d4ab3186f51c47d9a2fbd93bacef060c6c38e
Oscar Dominguez <dominguez.celada@gmail.com>
2021-04-20 16:48:23 +0200

    test(telegram): create e2e telegram (#128)
    * test(telegram): create e2e telegram
    
    It fails, needs to debug
    
    * fix(e2e): telegram test reads proper property from JSON
    
    * fix(telegram): use proper e2e url

---
+++ experiences/telegram/telegram.test.js
@@ -0,0 +1,79 @@
+const { bootstrapExperience } = require('../../e2e/utils/bootstrap');
+const { scrollTo } = require('../../e2e/utils/scroll');
+const { touchCard } = require('../../e2e/utils/touch');
+const { isAtSnapPoint } = require('../../e2e/utils/snapPoints');
+const { isCardContentLoaded } = require('../../e2e/utils/cardContent');
+const { expect } = require('chai');
+const { getUrlFixture } = require('../../e2e/utils/fixtureUrl');
+const experience = require('./telegram.json');
+
+describe('telegram experience', function() {
+	let config,
+		fixture;
+	const fixtureUrl = getUrlFixture({
+		siteUrl: 'https://playground.marfeel.com/templates/article-example.html',
+		requestHostname: 'playground.marfeel.com',
+		technology: 'web',
+		experienceUrl: '/experiences/telegram/telegram.json'
+	});
+
+	it('setup', async function() {
+		config = experience;
+		fixture = {
+			url: fixtureUrl,
+			articleTitle: 'Article example'
+		};
+
+		await bootstrapExperience(browser, config, fixture);
+	});
+
+	it('card should render on scroll', async function() {
+		await scrollTo(browser, 400);
+
+		const firstCard = await browser.$(config.cards.telegram.cardSelector);
+
+		const firstCardExists = await firstCard.waitForExist({ timeout: 5000 });
+
+		expect(firstCardExists).equal(true);
+	});
+
+	it('card should have right content', async function() {
+		const rightContentLoaded = await isCardContentLoaded(
+			browser,
+			config.cards.telegram.cardSelector,
+			config.cards.telegram.content
+		);
+
+		expect(rightContentLoaded).equal(true);
+	});
+
+	it('card should be displayed in viewport at initial snap point', async() => {
+		await scrollTo(browser, 800);
+
+		const firstCard = await browser.$(config.cards.telegram.cardSelector);
+
+		const firstCardIsInViewport = await firstCard.isDisplayedInViewport();
+
+		expect(firstCardIsInViewport).equal(true);
+
+		const isAtInitialSnapPoint = await isAtSnapPoint(
+			browser,
+			config.cards.telegram.cardSelector,
+			config.cards.telegram.snapPoints.initial
+		);
+
+		expect(isAtInitialSnapPoint).equal(true);
+	});
+
+	it('activate card by click', async() => {
+		await touchCard(browser, config.cards.telegram.cardSelector);
+
+		const isAtActiveSnapPoint = await isAtSnapPoint(
+			browser,
+			config.cards.telegram.cardSelector,
+			config.cards.telegram.snapPoints.active
+		);
+
+		expect(isAtActiveSnapPoint).equal(true);
+	});
+});
