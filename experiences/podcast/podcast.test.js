f4a1d163bd46d75ed054a002626274b6e523b254
Oscar Dominguez <dominguez.celada@gmail.com>
2021-04-20 15:31:45 +0200

    test(podcast): add e2e podcast (#131)
    * test(podcast): add e2e podcast
    
    * fix(podcast): use proper html for e2e
    
    * chore(paywall): remove comment

---
+++ experiences/podcast/podcast.test.js
@@ -0,0 +1,79 @@
+const { bootstrapExperience } = require('../../e2e/utils/bootstrap');
+const { scrollTo } = require('../../e2e/utils/scroll');
+const { touchCard } = require('../../e2e/utils/touch');
+const { isAtSnapPoint } = require('../../e2e/utils/snapPoints');
+const { isCardContentLoaded } = require('../../e2e/utils/cardContent');
+const { expect } = require('chai');
+const { getUrlFixture } = require('../../e2e/utils/fixtureUrl');
+const experience = require('./podcast.json');
+
+describe('podcast experience', function() {
+	let config,
+		fixture;
+	const fixtureUrl = getUrlFixture({
+		siteUrl: 'https://playground.marfeel.com/templates/article-example.html',
+		requestHostname: 'playground.marfeel.com',
+		technology: 'web',
+		experienceUrl: '/experiences/podcast/podcast.json'
+	});
+
+	it('setup', async function() {
+		config = experience;
+		fixture = {
+			url: fixtureUrl,
+			articleTitle: 'Article example'
+		};
+
+		await bootstrapExperience(browser, config, fixture);
+	});
+
+	it('card should render on scroll', async function() {
+		await scrollTo(browser, 400);
+
+		const firstCard = await browser.$(config.cards.podcast.cardSelector);
+
+		const firstCardExists = await firstCard.waitForExist({ timeout: 5000 });
+
+		expect(firstCardExists).equal(true);
+	});
+
+	it('card should have right content', async function() {
+		const rightContentLoaded = await isCardContentLoaded(
+			browser,
+			config.cards.podcast.cardSelector,
+			config.cards.podcast.content
+		);
+
+		expect(rightContentLoaded).equal(true);
+	});
+
+	it('card should be displayed in viewport at initial snap point', async() => {
+		await scrollTo(browser, 800);
+
+		const firstCard = await browser.$(config.cards.podcast.cardSelector);
+
+		const firstCardIsInViewport = await firstCard.isDisplayedInViewport();
+
+		expect(firstCardIsInViewport).equal(true);
+
+		const isAtInitialSnapPoint = await isAtSnapPoint(
+			browser,
+			config.cards.podcast.cardSelector,
+			config.cards.podcast.snapPoints.initial
+		);
+
+		expect(isAtInitialSnapPoint).equal(true);
+	});
+
+	it('activate card by click', async() => {
+		await touchCard(browser, config.cards.podcast.cardSelector);
+
+		const isAtActiveSnapPoint = await isAtSnapPoint(
+			browser,
+			config.cards.podcast.cardSelector,
+			config.cards.podcast.snapPoints.active
+		);
+
+		expect(isAtActiveSnapPoint).equal(true);
+	});
+});
