6cef22bc2614b150f2a1c839e2b60f32828f4edb
David Valentín <davidvalentin@MacBook-Pro-50.local>
2021-04-20 10:52:00 +0200

    feat(e2e): branded content, comments, facebook e2e tests

--- experiences/comments/comments.test.js
+++ experiences/comments/comments.test.js
@@ -11,7 +11,7 @@ describe('comments card experience', function() {
 	let config,
 		fixture;
 	const fixtureUrl = getUrlFixture({
-		siteUrl: 'https://playground.marfeel.com/templates/article-skeleton-web.html',
+		siteUrl: 'https://playground.marfeel.com/templates/article-example.html',
 		requestHostname: 'playground.marfeel.com',
 		technology: 'web',
 		experienceUrl: '/experiences/comments/comments.json'
