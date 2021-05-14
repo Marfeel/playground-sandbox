6ac4f5ae8150eb1acad5e88709d654fc9a5a0fb8
Lautaro Rodriguez Garrido <lautaro.rodriguez@marfeel.com>
2021-05-14 15:36:44 +0200

    touches inside card done using click

--- e2e/utils/touch.js
+++ e2e/utils/touch.js
@@ -109,41 +109,10 @@ const touchCard = async(browser, cardSelector)=>{
 
 const touchElementInsideCard = async(browser, cardSelector, childElementSelector)=>{
 	await browser.execute(async(cardSelectorBrowser, childElementSelectorBrowser) => {
-		const createTouch = (target, clientY, clientX = 200) => {
-			return new Touch({
-				identifier: 123,
-				target,
-				clientY,
-				clientX,
-				radiusX: 2.5,
-				radiusY: 2.5,
-				rotationAngle: 10,
-				force: 0.5
-			});
-		};
-
-		const touch = async(el, y1, x1) => {
-			const touchStart = createTouch(el, y1, x1);
-			const touchEnd = createTouch(el, y1, x1);
-
-			const touchStartEvent = new TouchEvent('touchstart', { bubbles: true, touches: [touchStart] });
-			const touchEndEvent = new TouchEvent('touchend', { bubbles: true, touches: [touchEnd] });
-
-			await el.dispatchEvent(touchStartEvent);
-			await el.dispatchEvent(touchEndEvent);
-		};
-
-		const touchElementAt = async(el) => {
-			const { y, x } = await el.getBoundingClientRect();
-
-			await touch(el, y + 5, x + 5);
-		};
-
 		const cardDocument = document
 			.querySelector(`${cardSelectorBrowser} article [data-testid="amp-document-single"]`);
 		const element = cardDocument.shadowRoot.querySelector(childElementSelectorBrowser);
-
-		await touchElementAt(element);
+		element.click();
 	}, cardSelector, childElementSelector);
 };
 
