a60fb7182b5ee496b62835603fe08c965cc32dde
Lautaro Rodriguez Garrido <lautaro.rodriguez@marfeel.com>
2021-05-27 16:45:07 +0200

    fix: relevant experiences not to run a deleted test

--- e2e/relevant-experiences.test.js
+++ e2e/relevant-experiences.test.js
@@ -3,10 +3,8 @@ const facebookTest = require('../experiences/facebook/facebook.test');
 const featuredArticleTest = require('../experiences/featured_article/featured-article.test');
 const homepageTest = require('../experiences/homepage/homepage.test');
 const multicardTest = require('../experiences/multicard/multicard.test');
-const paywallTest = require('../experiences/paywall/paywall.test');
 
 describe('homepage experience', homepageTest);
 describe('facebook experience', facebookTest);
 describe('featured article experience', featuredArticleTest);
 describe('multicard experience', multicardTest);
-describe('paywall experience', paywallTest);
