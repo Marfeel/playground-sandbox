6fb25ac0cb22ec43f5b40af95a505996ec4c9cd2
Òscar Marí <50138731+oscarmarimrf@users.noreply.github.com>
2021-06-04 12:21:32 +0200

    fix(branded-content): wait for DFP reponse to initalize flowcards

--- experiences/branded-content/branded-content.js
+++ experiences/branded-content/branded-content.js
@@ -8,31 +8,20 @@
  window.marfeel.cmd = window.marfeel.cmd || [];
 
 function requestBrandedContent() {
-  return new Promise(resolve => {
     window.googletag = window.googletag || { cmd: [] };
     window.googletag.cmd.push(function () {
       window.googletag.defineOutOfPageSlot('/22571134/crispr/demo.marfeel.com', 'flowcards-slot').addService(googletag.pubads());
       window.googletag.enableServices();
       window.googletag.display('flowcards-slot');
-  
-      resolve();
-    });
   });
 }
 
-window.marfeel.cmd.push([
-  'flowcards',
-  (flowcards) => {
-    flowcards.addTargeting('template', 'branded-content');
-  }
-]);
+requestBrandedContent();
 
-const $brandedContent = requestBrandedContent();
-
- window.marfeel.config = {
+window.marfeel.config = {
   flowcards: {
     waitFor: {
-      promise: $brandedContent
+      timeout: 2000
     }
   }
  };
